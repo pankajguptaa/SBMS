31a545547ad8aef0025eb57193d2caa7f0a48489
Pankaj <guptaapankaj21@gmail.com>
2024-12-14 17:35:10 +0530

    Add A file

---
+++ A.java
@@ -0,0 +1,16 @@
+class A{
+	public static void main(String[] args){
+		System.out.println("A class.");		
+	}
+}
+class B{
+	public static void main(String[] args){
+		System.out.println("B class.");
+		A.main(new String[0]);
+	}
+}
+class C{
+	public static void main(String[] args) {
+		System.out.println("C class ");
+	}
+}
